15551ade02b821bdb84bcac4028f11f9981b487c
zhangyurui <1057435191@qq.com>
2021-05-24 20:11:22 +0800

    hot-fix test

--- src/main/java/com/bjpowernode/git/GitTest.java
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -3,5 +3,7 @@ package com.bjpowernode.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellogit");
+        System.out.println("hellogit1");
+        System.out.println("hellogit2");
     }
 }
