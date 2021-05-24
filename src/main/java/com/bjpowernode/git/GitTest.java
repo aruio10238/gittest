961573bdfc61bb27960f4644beffef19e019f5a3
zhangyurui <1057435191@qq.com>
2021-05-24 20:19:54 +0800

    push commit

--- src/main/java/com/bjpowernode/git/GitTest.java
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hellogit2");
         System.out.println("hellogit1");
         System.out.println("hellogit2");
+        System.out.println("push hellogit2");
     }
 }
